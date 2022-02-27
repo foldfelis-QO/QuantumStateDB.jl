559a4f2a26b635f920c1cea9978792cea2910630
JingYu Ning <foldfelis@gmail.com>
2022-02-27 12:52:35 +0800

    Implement array utils to psql

--- src/table.jl
+++ src/table.jl
@@ -6,6 +6,20 @@ struct SqueezedThermalStates <: QuantumStates end
 Base.string(::Type{SqueezedStates}) = "squeezed_states"
 Base.string(::Type{SqueezedThermalStates}) = "squeezed_thermal_states"
 
+ρ2psql(m::AbstractMatrix) = "'" * replace(string([m[i, :] for i in 1:size(m, 1)]), '['=>'{', ']'=>'}') * "'"
+
+function p2psql(p::AbstractMatrix)
+    θs, xs = p[1, :], p[2, :]
+
+    return replace(
+        string(collect(zip(θs, xs))),
+        "[" => "'{",
+        "]" => "}'",
+        "(" => "\"(",
+        ")" => ")\""
+    )
+end
+
 function generate_create_table(::Type{SqueezedStates})
     return """
     CREATE TABLE $(string(table)) (
